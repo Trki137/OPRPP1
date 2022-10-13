c1d4e6774ca1097ec93d8b15fe46d4d1c26ebfc2
Dean <71371601+Trki137@users.noreply.github.com>
2022-10-13 14:04:34 +0200

    ArrayIndexedCollection javaDoc written

--- src/main/java/hr/fer/oprpp1/custom/collections/ArrayIndexedCollection.java
+++ src/main/java/hr/fer/oprpp1/custom/collections/ArrayIndexedCollection.java
@@ -1,16 +1,38 @@
 package hr.fer.oprpp1.custom.collections;
 
-import java.nio.charset.UnsupportedCharsetException;
 import java.util.Arrays;
+
+/**
+ *
+ * @author Dean Trkulja
+ * @version 1.0
+ *
+ *
+ * */
 
 public class ArrayIndexedCollection extends Collection{
 
-    private int size; //current size of collection
+    /**
+     * Number of element stored in {@code elements}
+     * Value can be from 0 to elements lenght
+     */
+    private int size;
+    /**
+     * Array which stores values
+     */
     private Object[] elements;
+    /**
+     * Default size of {@code elements} when it's not specified by user
+     */
     private static final int DEFAULT_ARRAY_SIZE = 16;
 
 
-
+    /**
+     *
+     * @param collection elements of this collection are copied into our new collection
+     * @param initialSize initial size of our collection
+     *
+     */
     public ArrayIndexedCollection(Collection collection, int initialSize){
         if(initialSize < 1) throw new IllegalArgumentException();
         if(collection == null) throw new NullPointerException();
@@ -19,13 +41,22 @@ public class ArrayIndexedCollection extends Collection{
         addAll(collection);
 
         this.size = collection.size();
-
-
     }
+
+    /**
+     * This constructor delegates it's job to more complex constructor with default size
+     * @param collection elements of this collection are copied into our new collection
+     *
+     */
 
     public ArrayIndexedCollection(Collection collection){
         this(collection,DEFAULT_ARRAY_SIZE);
     }
+
+    /**
+     * Initializes an array of object
+     * @param initialCapacity initial size of our collection
+     */
 
     public ArrayIndexedCollection(int initialCapacity){
         if(initialCapacity < 1) throw new IllegalArgumentException();
@@ -34,13 +65,26 @@ public class ArrayIndexedCollection extends Collection{
         elements = new Object[initialCapacity];
     }
 
+    /**
+     * Default constructor that delegates it's job to more complex constructor
+     * It will create a collction that has size of 16
+     */
     public ArrayIndexedCollection(){
         this(DEFAULT_ARRAY_SIZE);
     }
 
 
-
-
+    /**
+     *
+     * Inserts value in specific position
+     * Throws 2 exceptions:
+     *    - IndexOutOfBoundsException is thrown when position is greater then size of collecton or smaller then 0
+     *    - NullPointerException is thrown when null value occurs in {@param value}
+     *
+     * @param value value that we store in our collection
+     * @param position in which position do we want to store element, eligible value is between 0 and current size of collection
+     *
+     */
     public void insert(Object value, int position){
         if(position < 0 || position > size) throw new IndexOutOfBoundsException();
         if(value == null) throw new NullPointerException();
@@ -66,11 +110,27 @@ public class ArrayIndexedCollection extends Collection{
         }
     }
 
+    /**
+     * Returns value of collection in position {@param index}
+     * Throws IndexOutOfBoundsException when index is smaller then 0 or greater then size
+     *
+     * @param index specifies index of element that we want to get
+     * @return element which is located at position {@param index}
+     *
+     */
+
     public Object get(int index){
         if(index >= 0 && index <= size-1) return elements[index];
         throw new IndexOutOfBoundsException();
     }
 
+    /**
+     * Returns index of element that we are searching
+     *
+     * @param value element that we are searching for
+     * @return -1 if element doesn't exists or value is null
+     *          Index of element if elements exists
+     */
     public int indexOf(Object value){
         if(value == null) return -1;
 
@@ -80,6 +140,12 @@ public class ArrayIndexedCollection extends Collection{
         return -1;
     }
 
+    /**
+     * Removes element in a way that shifts element to the left by one position from index value and last element is set to null
+     * Throws IllegalArgumentException when index is greater then size - 1 or smaller then 0
+     *
+     * @param index element which is located at position {@param index} is deleted
+     */
     public void remove(int index){
         if(index < 0 || index > size - 1) throw new IllegalArgumentException();
 
@@ -93,16 +159,33 @@ public class ArrayIndexedCollection extends Collection{
         size--;
     }
 
+    /**
+     *
+     * @return site of collection
+     */
     @Override
     public int size(){
         return  this.size;
     }
 
+    /**
+     * Checks if element exists in collection
+     *
+     * @param value element that we are searching for in our collection
+     * @return true if exists else false
+     *
+     */
     @Override
     public boolean contains(Object value){
         return indexOf(value) != -1;
     }
 
+    /**
+     * Removes first element in collection which has value equal to {@param value}
+     *
+     * @param value
+     * @return true if element is removed in other cases return false
+     */
     @Override
     public boolean remove(Object value){
 
@@ -112,11 +195,24 @@ public class ArrayIndexedCollection extends Collection{
         return true;
     }
 
-    @Override
 
+
+    /**
+     * copies our collection to array
+     * @return array of objects
+     */
+    @Override
     public Object[] toArray(){
         return Arrays.copyOf(elements, size);
     }
+
+    /**
+     * Adds new element to collection to first available spot in {@code elements}
+     * If {@code elements} doesn't have space anymore we create new array with double capacity and copy values from old array to new
+     * Throws NullPointerException if {@param value} is null
+     *
+     * @param value value that we want to store in collection
+     */
 
     @Override
     public void add(Object value){
@@ -135,6 +231,9 @@ public class ArrayIndexedCollection extends Collection{
 
     }
 
+    /**
+     * Clears {@code elements} and sets size to 0
+     */
     @Override
     public void clear(){
 
@@ -144,6 +243,10 @@ public class ArrayIndexedCollection extends Collection{
 
     }
 
+    /**
+     * For each value in collection we call proccesor.process
+     * @param processor instance of class Processor
+     */
     @Override
     public void forEach(Processor processor){
 
