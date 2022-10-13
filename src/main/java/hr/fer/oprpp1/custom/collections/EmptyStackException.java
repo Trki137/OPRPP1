6bae2295dc6d26d36a416b5d4f2f9c2bedcc64fa
Dean <71371601+Trki137@users.noreply.github.com>
2022-10-13 18:11:29 +0200

    EmptyStackException javaDoc written

--- src/main/java/hr/fer/oprpp1/custom/collections/EmptyStackException.java
+++ src/main/java/hr/fer/oprpp1/custom/collections/EmptyStackException.java
@@ -1,9 +1,27 @@
 package hr.fer.oprpp1.custom.collections;
 
+/**
+ * Implementation of own Runtime exception
+ * Will use for our stack implementation
+ *
+ * @author Dean Trkulja
+ * @version 1.0
+ *
+ */
 public class EmptyStackException extends RuntimeException{
+
+    /**
+     * Default constructor
+     */
     public EmptyStackException(){
         super();
     }
+
+    /**
+     * Constructor with message problem
+     *
+     * @param description for message to user
+     */
     public EmptyStackException(String description){
         super(description);
     }
