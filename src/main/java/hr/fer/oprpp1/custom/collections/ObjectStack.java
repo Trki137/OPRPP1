c81abacde9fb25d11ac7891a19fd079e0e2a3358
Dean <71371601+Trki137@users.noreply.github.com>
2022-10-13 14:37:15 +0200

    ObjectStack javaDoc written

--- src/main/java/hr/fer/oprpp1/custom/collections/ObjectStack.java
+++ src/main/java/hr/fer/oprpp1/custom/collections/ObjectStack.java
@@ -1,21 +1,52 @@
 package hr.fer.oprpp1.custom.collections;
 
+/**
+ *
+ * Class that implements stack via our own ArrayIndexCollection
+ *
+ * @author Dean Trkulja
+ * @version 1.0
+ */
 
 
 public class ObjectStack {
+    /**
+     * instance of ArrayIndexedCollection will represent in some ways stack
+     */
     ArrayIndexedCollection stack;
 
+    /**
+     * Initializes stack
+     */
     public ObjectStack(){
         stack = new ArrayIndexedCollection();
     }
+
+    /**
+     * Checks if stack is empty or not
+     *
+     * @return true if empty else false
+     */
 
     public boolean isEmpty(){
         return stack.isEmpty();
     }
 
+    /**
+     * @return number of elements in stack
+     */
+
     public int size(){
         return stack.size();
     }
+
+    /**
+     * Stores element at top of stack
+     *
+     * Throws NullPointerException if {@param value} is null
+     *
+     * @param value elemnt that we want to store in stack
+     */
 
     public void push(Object value){
         if(value == null)throw new NullPointerException();
@@ -23,6 +54,11 @@ public class ObjectStack {
         stack.add(value);
     }
 
+    /**
+     * Removes element from to of stack
+     *
+     * @return element from top of stack
+     */
     public Object pop(){
         if(this.size() == 0) throw new EmptyStackException();
         Object elem = peek();
@@ -30,11 +66,19 @@ public class ObjectStack {
         return elem;
     }
 
+    /**
+     * Return element from top of stack, but it is not deleted
+     *
+     * @return element from top of stack
+     */
     public Object peek(){
         if(this.size() == 0) throw new EmptyStackException();
         return stack.get(size() - 1);
     }
 
+    /**
+     * Removes all elements from stack
+     */
     public void clear(){
         stack.clear();
     }
