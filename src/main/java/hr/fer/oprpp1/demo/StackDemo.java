338bef65dc4f523c380d94556a3029143240bf6d
Dean <71371601+Trki137@users.noreply.github.com>
2022-10-13 13:17:25 +0200

    Stacked Demo implementation improved with regex

--- src/main/java/hr/fer/oprpp1/demo/StackDemo.java
+++ src/main/java/hr/fer/oprpp1/demo/StackDemo.java
@@ -1,25 +1,20 @@
 package hr.fer.oprpp1.demo;
 
-import hr.fer.oprpp1.custom.collections.ArrayIndexedCollection;
 import hr.fer.oprpp1.custom.collections.ObjectStack;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class StackDemo {
     public static void main(String[] args) {
         if(args.length != 1) throw new IllegalArgumentException("Argument not valid");
         String[] arr = args[0].split(" ");
-        List<String> operands = Arrays.asList(new String[]{"+","-", "*", "/", "%"});
         ObjectStack stack = new ObjectStack();
-        calculate(arr, operands, stack);
+        calculate(arr, stack);
         if (stack.size() != 1) System.out.println("Something went wrong");
         else System.out.println("Expression evaluates to "+ stack.pop() + ".");
     }
 
-    private static void calculate(String[] arr, List<String> operands, ObjectStack stack) {
+    private static void calculate(String[] arr, ObjectStack stack) {
         for(String element : arr) {
-            if (!operands.contains(element)) stack.push(element);
+            if (!element.matches("[+\\-/*%]")) stack.push(element);
             else{
 
                 int value2 = Integer.parseInt(stack.pop().toString());
